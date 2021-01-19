b5a285ad075b01473821d28db2b0863cd301c3e5
Bakanette <45857178+Bakanette@users.noreply.github.com>
2021-01-19 23:01:42 +0100

    Font import

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,8 @@ export default {
       { hid: 'description', name: 'description', content: '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: "preconnect", href: "https://fonts.googleapis.com/css2?family=Oswald&family=Roboto&display=swap"}
     ]
   },
 
@@ -35,5 +36,6 @@ export default {
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
-  }
+  },
+  css: ["~/assets/styles.css"]
 }
