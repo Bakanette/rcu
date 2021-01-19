010b347b9cb5b9b434ae79ef8f9b56970b72933f
Bakanette <45857178+Bakanette@users.noreply.github.com>
2021-01-19 01:16:11 +0100

    RCU Init

---
+++ api/posts.js
@@ -0,0 +1,18 @@
+import GhostContentAPI from "@tryghost/content-api";
+
+const api = new GhostContentAPI({
+    url: 'https://bakanne.fr',
+    key: 'e77cc90315096b672fdb747c5a',
+    version: "v2"
+  });
+
+  export async function getPosts() {
+    return await api.posts
+    .browse({
+      limit: "all",
+      include: "tags,authors",
+    })
+    .catch(err => {
+      console.error(err);
+    })
+  }
---
+++ layouts/default.vue
@@ -0,0 +1,78 @@
+<template>
+  <div>
+    <header>
+      <div class="container">
+        <div class="navbar">
+          <nuxt-link to="/" class="logo"><img src="../img/logo_rcu_small.png" alt="Logo RCU"> </nuxt-link>
+          <nav>
+            <ul>
+              <li><nuxt-link to="/actualite">Actualité</nuxt-link></li>
+              <li><nuxt-link to="/equipes">Les équipes</nuxt-link></li>
+              <li><nuxt-link to="/ecole">L'école de rugby</nuxt-link></li>
+              <li><nuxt-link to="/partenaires">Les partenaires</nuxt-link></li>
+              <li><nuxt-link to="/contacts">Contacts</nuxt-link></li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </header>
+    <nuxt/>
+  </div>
+</template>
+
+<style>
+html {
+  font-family:
+    'Source Sans Pro',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+.button--green {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #3b8070;
+  color: #3b8070;
+  text-decoration: none;
+  padding: 10px 30px;
+}
+
+.button--green:hover {
+  color: #fff;
+  background-color: #3b8070;
+}
+
+.button--grey {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #35495e;
+  color: #35495e;
+  text-decoration: none;
+  padding: 10px 30px;
+  margin-left: 15px;
+}
+
+.button--grey:hover {
+  color: #fff;
+  background-color: #35495e;
+}
+</style>
