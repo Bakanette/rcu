10635af41a592fa2d543465a057caff8712a6928
Bakanette <45857178+Bakanette@users.noreply.github.com>
2021-01-20 13:22:07 +0100

    Fix Font

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,8 +14,7 @@ export default {
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [
-  ],
+  css: ["~/assets/styles.css"],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
@@ -36,6 +35,5 @@ export default {
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
-  },
-  css: ["~/assets/styles.css"]
+  }
 }
