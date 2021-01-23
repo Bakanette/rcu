1258c8f2c1f4783f94e83609cb01aebc39b4a546
Bakanette <45857178+Bakanette@users.noreply.github.com>
2021-01-23 05:58:55 +0100

    Commit

--- pages/_slug/index.vue
+++ pages/_slug/index.vue
@@ -1,44 +1,54 @@
 <template>
+<client-only>
     <div class="container">
-      <main>
-        <h1 class="title-post">{{ post.title }}</h1>
-        <div class="content-post">
-            <div v-html="post.html">{{ post.html }}</div>
-        </div>
-      </main>
+        <main>
+            <h1>{{ post.title }}</h1>
+            <div class="content">
+                <div v-html="post.html">{{ post.html }}</div>
+            </div>
+        </main>
     </div>
+</client-only>
 </template>
 
 <script>
+
 import { getSinglePost } from '~/api/posts';
 
 export default {
-  async asyncData ({params}) 
-  {
-    const post = await getSinglePost(params.slug);   
+  async asyncData ({params}) {
+    const post = await getSinglePost(params.slug);
     return { post: post }
   }
 }
 </script>
 
-<style>
-    .title-post {
-        padding-top: 1em;
+<style lang="scss">
+    header {
+        height: 15em;
+    }
+
+    h1 {
         color: white;
         margin-bottom: 1em;
     }
 
-    .content-post {
+    .content {
+        background: white;
         border-radius: 1em;
         padding: 1em;
     }
 
-    .content-post img {
+    main {
+        margin-top: -9em;
+    }
+
+    .content img {
         width: 100%;
     }
 
 @media only screen and (min-width: 768px) {
-    .content-post {
+    .content {
         padding: 2em;
     }
 }
