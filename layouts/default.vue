6d6da09f604a81ec478e7ffa4fdaf378ae4a1a63
Bakanette <45857178+Bakanette@users.noreply.github.com>
2021-02-02 17:54:31 +0100

    Add "Partenaires" pages

--- layouts/default.vue
+++ layouts/default.vue
@@ -14,11 +14,12 @@
     <b-collapse id="navbar-toggle-collapse" is-nav>
       <b-navbar-nav class="ml-auto">
         <b-nav-item href="/">Accueil</b-nav-item>
-        <b-nav-item href="/actualites">Les Actualités</b-nav-item>
+        <b-nav-item href="/actualites">Les actualités</b-nav-item>
         <b-nav-item href="/equipes">Les équipes</b-nav-item>
         <b-nav-item href="/ecole">L'école de rugby</b-nav-item>
         <b-nav-item href="/staff">Le staff</b-nav-item>
         <b-nav-item href="/galeriePhotos">Galerie photos</b-nav-item>
+        <b-nav-item href="/partenaires">Les partenaires</b-nav-item>
       </b-navbar-nav>
     </b-collapse>
   </b-navbar>
